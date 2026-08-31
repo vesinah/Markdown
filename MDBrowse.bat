b7fb55f55b40351c6e77f24b87526e62b4c81e6c
Vesin <vesin@local>
2026-08-31 18:35:59 +0700

    เปลี่ยนเป็น local server

--- MDBrowse.bat
+++ MDBrowse.bat
@@ -1,2 +1,6 @@
 @echo off
-start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --app=file:///D:/01_APP/Markdown/MDBrowse.html
+REM Start local server in background on port 8080
+start /B "" npx serve "D:\01_APP\Markdown" -p 8080 --no-clipboard >nul 2>&1
+timeout /t 2 /nobreak >nul
+REM Open Chrome in app mode via localhost
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --app=http://localhost:8080/MDBrowse.html
